565f6fbecdfba517a43d2898e533174638123a9f
cheezer <subunc5@gmail.com>
2024-12-24 19:14:17 +0530

    REPL

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main()  {
-    fmt.Printf("Hello, World!")
+	for {
+		fmt.Print("Pokedex > ")
+		s := bufio.NewScanner(os.Stdin)
+		if s.Scan() {
+			op := cleanInput(s.Text())
+			fmt.Printf("Your command was: %v\n", op[0])
+		}
+	}
 }
 
 func cleanInput(text string) []string {
