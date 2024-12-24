59c7d7b53e717b9069b6029439cbfbd1b85dc015
cheezer <subunc5@gmail.com>
2024-12-24 20:04:36 +0530

    add help and exit commands

--- main.go
+++ main.go
@@ -7,17 +7,60 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name string
+	description string
+	callback func() error
+}
+
+var cmdReg map[string]cliCommand
+
 func main()  {
+	//registry of valid commands
+	cmdReg = map[string]cliCommand{
+		"exit": {
+			name: "exit",
+			description: "Exit the Pokedex",
+			callback: commandExit,
+		},
+		"help": {
+			name: "help",
+			description: "Display a help message",
+			callback: commandHelp,
+		},
+	}
+	s := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
-		s := bufio.NewScanner(os.Stdin)
 		if s.Scan() {
-			op := cleanInput(s.Text())
-			fmt.Printf("Your command was: %v\n", op[0])
+			inp := cleanInput(s.Text())[0];
+			cmd, exists := cmdReg[inp]
+			if exists {
+				cmd.callback()
+			} else {
+				fmt.Println("Unknown command")
+			}
 		}
+
 	}
 }
 
 func cleanInput(text string) []string {
 	return strings.Fields(strings.ToLower(text))
+}
+
+//callback for commands
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Printf("Welcome to the Pokedex!\nUsage:\n\n")
+	for cmd, val := range cmdReg {
+		fmt.Printf("%v: %v\n", cmd, val.description)
+	}
+	return nil
 }
